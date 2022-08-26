51bd4a6654e1cb24556da231169112e99d9d4108
siddarth patel <siddarth.patel@ibm.com>
2022-08-26 13:28:28 -0400

    complete searchRange in an array

--- SORTING-SEARCHING/searchRange.js
+++ SORTING-SEARCHING/searchRange.js
@@ -5,22 +5,43 @@ const searchRange = (nums, target) => {
 
     if (nums.length === 1 && nums[left] === target) return [left, left]
 
+    // get the firstOccurIdx 
     while (left <= right) {
         let mid = parseInt((left + right)/2)
         if (nums[mid] === target) {
-            (nums[mid - 1] === target) ? firstOccurIdx = mid - 1 : firstOccurIdx = mid
-            if (nums[firstOccurIdx + 1] === target) return [firstOccurIdx, firstOccurIdx + 1]
-            else return [firstOccurIdx, firstOccurIdx]
+            if (nums[mid - 1] !== target) {
+                firstOccurIdx = mid
+                break
+            }
+            else right = mid - 1 
         }
         else if (nums[mid] < target) left = mid + 1
         else right = mid - 1
     }
 
-    return [-1, -1]
+    left = 0
+    right = nums.length - 1
+    let lastOccurIdx = -1
+
+    // get the secondOccurIdx 
+    while (left <= right) {
+        let mid = parseInt((left + right)/2)
+        if (nums[mid] === target) {
+            if (nums[mid + 1] !== target) {
+                lastOccurIdx = mid
+                break
+            }
+            else left = mid + 1 
+        }
+        else if (nums[mid] < target) left = mid + 1
+        else right = mid - 1
+    }
+    return [firstOccurIdx, lastOccurIdx]
 }   
 
-console.log(searchRange([5,7,7,8,8,10,11,11,12,13], 7))
-console.log(searchRange([], 0))
-console.log(searchRange([5,7,7,8,8,10], 6))
-console.log(searchRange([1, 0], 1))
+// console.log(searchRange([5,7,7,8,8,10,11,11,12,13], 7))
+// console.log(searchRange([], 0))
+// console.log(searchRange([5,7,7,8,8,10], 6))
+// console.log(searchRange([1, 0], 1))
+console.log(searchRange([2, 7, 7, 7, 8, 9], 7))
 
