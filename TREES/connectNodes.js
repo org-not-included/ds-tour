7fe6f11bdde1d7ce2ebdc977954c52609af84867
Siddarth Patel <siddarthpatel@Siddarths-MacBook-Pro.local>
2022-08-28 12:09:28 -0400

    pass test cases connectNodes TREES

--- TREES/connectNodes.js
+++ TREES/connectNodes.js
@@ -8,6 +8,7 @@ function Node(val, left, right, next) {
 let root = new Node(1, new Node(2, new Node(4), new Node(5)), new Node(3, new Node(6), new Node(7)))
 
 const connect = (root) => {
+    if (!root) return null
     let queue = [root]
     let levels = []
     while(queue.length !== 0){
@@ -15,19 +16,19 @@ const connect = (root) => {
         let level = []
         for (var levelIndex=0; levelIndex<levelLength; levelIndex++){
             let node = queue.shift()
-            if (levelLength === 1 || levelIndex === levelLength - 1) node.next === null
+            if (levelLength === 1 || levelIndex === levelLength - 1) node.next = null
             else node.next = queue[0]
             // push nodes to a level in queue
-            if (node.left) queue.push(node.left)
-            if (node.right) queue.push(node.right)
+            if (node && node.left) queue.push(node.left)
+            if (node && node.right) queue.push(node.right)
             //add node or node.val to level
             level.push(node.val)
         }
         level.push('#')
-        levels.push(...level)        
+        levels.push(...level)     
     }
     console.log(levels)
-    console.log(root)
+    return root
 }
 
 connect(root)
