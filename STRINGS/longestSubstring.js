ca7ab573f47fda91dcdac6d8dddc948953ea1aa9
Siddarth Patel <siddarthpatel@Siddarths-MacBook-Pro.local>
2022-08-29 00:20:20 -0400

    update longestSubstring.js

--- STRINGS/longestSubstring.js
+++ STRINGS/longestSubstring.js
@@ -1,17 +1,20 @@
 const longestSubstring = (str) => {
-    let map = new Map()
-    let subStr = ''
-    let subStrLength = 0
-    for (var i=0; i<str.length; i++){
-        if (map.has(str.charAt(i))) {
-            subStr = ''
-            map.set(str.charAt(i), map.get(str.charAt(i)) + 1)
-        }
-        subStr = subStr + str.charAt(i)
-        subStrLength = Math.max(subStrLength, subStr.length)
-        map.set(str.charAt(i), 1)
+    var letters = str.split("");
+    var max = 0;
+    var result = new Map();
+
+    for (var i = 0; i < letters.length; i++) {
+      if (!result.has(letters[i])) {
+        result.set(letters[i], i);
+      } else {
+        i = result.get(letters[i]);
+        result.clear();
+      }
+      if (max < result.size) {
+        max = result.size;
+      }
     }
-    return subStrLength
+    return max;
 }
 
-console.log(longestSubstring('abcedbchijklmnoppe'))
+console.log(longestSubstring('dvdf'))
