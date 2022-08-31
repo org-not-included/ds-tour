511196630410f87aea1d991b5d7847e2cd15687c
siddarth patel <siddarth.patel@ibm.com>
2022-08-31 14:22:59 -0400

    add mergeTwoArrays.js

--- SORTING-SEARCHING/mergeTwoArrays.js
+++ SORTING-SEARCHING/mergeTwoArrays.js
@@ -6,35 +6,48 @@ function mergeTwoArrays(nums1, m, nums2, n){
     console.log(nums1)
 }
 
+//Space complexity O(m), Time complexity O(m + n)
 function twoPointer(nums1, m, nums2, n) {
-    if (m === 0 && n === 1){
-       nums1[0] = nums2[0]
-       console.log(nums1)
-       return nums1
-    } else if(m === 1 && n === 0) {
-        console.log(nums1)
-        return nums1
+    // Make a copy of the first m elements of nums1.
+    let nums1_copy = new Array(m)
+    for (var i=0; i<m; i++) nums1_copy[i] = nums1[i]
+    
+    // Read pointers for nums1Copy and nums2 respectively.
+    let p1=0
+    let p2=0
+    
+    // Compare elements from nums1Copy and nums2 and write the smallest to nums1.
+    for (var i=0; i<m+n; i++){
+        if (p2 >= n || (p1 < m && nums1_copy[p1] < nums2[p2]))
+            nums1[i] = nums1_copy[p1++]
+        else 
+            nums1[i] = nums2[p2++]
     }
-
-    var p1 = 0;
-    var p2 = 0;
-    var numsCopy = new Array(m)
-
-    for (var i = 0; i < m; i++) {
-        numsCopy[i] = nums1[i];
-    }
-
-    for (var i=0 ; i<m+n; i++){
-        if (numsCopy[p1] < nums2[p2]) {
-            nums1[i] = numsCopy[p1]
-            p1++
-        } else {
-            nums1[i] = nums2[p2]
-            p2++
-        }
-    }
-    console.log(nums1)
+    
+    return nums1
 }
 
+//Space complexity O(1), Time complexity O(m + n)
+function twoPointer2(nums1, m, nums2, n) {
+    let p1 = m - 1
+    let p2 = n - 1 
+
+    for (var p = m + n - 1; p >= 0; p--) {
+        if (p2 < 0) {
+            break;
+        }
+        if (p1 >= 0 && nums1[p1] > nums2[p2])
+            nums1[p] = nums1[p1--]
+        else
+            nums1[p] = nums2[p2--]
+    }
+
+    return nums1
+
+}
+
+
+
 // mergeTwoArrays([1,2,3,0,0,0], 3, [2,5,6], 3)
-twoPointer([0], 0, [1], 1)
+// twoPointer([0], 0, [1], 1)
+console.log(twoPointer([2,5,6,0,0,0], 3, [1,2,3], 3))
