7f2ddd6491bd89e0f1c62133c4180de75f9da7da
Siddarth Patel <siddarthpatel@Siddarths-MacBook-Pro-2.local>
2022-09-22 11:26:45 -0400

    update palindrome

--- STRINGS/palindrome.js
+++ STRINGS/palindrome.js
@@ -1,28 +1,12 @@
 // const str = "abc's test#s";
 // console.log(str.replace(/[^a-zA-Z ]/g, ""));
 
-helper = (low, high, str) => {
-    if (low >= high) {
-        return 
-    }
-    var tmp = str[low]
-    str[low] = str[high]
-    str[high] = tmp
-    helper(low + 1, high - 1, str)
-}
-
-reverseString = (str) => {
-    helper(0, str.length - 1, str)
-    return str
-}
-
 var isPalindrome = function(s) {
-    let str = s.replace(/[^a-zA-Z]/g, "")
-    console.log(str)
-    let revStr = reverseString(str.toLowerCase())
-    if (str.toLowerCase() === revStr)
+    let str = s.replace(/[^a-zA-Z0-9]/g, "")
+    let revStr = str.split("").reverse().join("");
+    if (str.toLowerCase() === revStr.toLowerCase())
         return true
-    return false
+    return false 
 }
 
 // console.log(isPalindrome("A man, a plan, a canal: Panama"))
